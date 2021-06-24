8c0bd4fded13afbd66c9ae2edd2e1161980c5f57
yuki <771634986@qq.com>
2021-06-24 15:52:27 +0800

    tailwind

---
+++ vite-01/src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded" > 
+  Button 
+</button>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
