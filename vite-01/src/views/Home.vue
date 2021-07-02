fca9a94d1303e60e5ec7803d4224c014f35f473e
tonssss <294343714@qq.com>
2021-07-02 10:36:54 +0800

    pc

--- vite-01/src/views/Home.vue
+++ vite-01/src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + Vite" />
-  </template>
+  hello world
+</template>
 
 <script setup>
 import HelloWorld from '../components/HelloWorld.vue'
