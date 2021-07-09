92e434d3508e3efa948f83aee9e782a0eb8e8059
yuki <771634986@qq.com>
2021-07-09 14:31:47 +0800

    tailwind

--- vite-01/src/views/toRef.vue
+++ vite-01/src/views/toRef.vue
@@ -1,27 +1,147 @@
 <template>
+  <!-- container会在不同屏幕断点下有不同的固定宽度 -->
+  <div class="container mx-auto">
+    <input type="radio" name="tab" id="iphone-12-pro" />
+    <input type="radio" name="tab" id="iphone-12" />
+    <input type="radio" name="tab" id="iphone-se" />
+    <!-- 盒子圆角加阴影 -->
+    <div class="p-8 rounded-xl shadow-md">
+      <div class="flex">
+        <div class="w-3/5 flex flex-col justify-center">
+          <div>
+            <h2 class="text-2xl font-bold">iphone 12 pro</h2>
+            <p class="mt-3">
+              我们的硬件和软件配合默契，协作无间。想将新 AirPods 与 iPhone
+              配对？轻轻一点就设置完毕。想和附近的朋友共享照片、视频或通讯录？隔空投送功能会在屏幕上列出他们的名字，只要动动手指选中就能搞定。
+            </p>
+            <button
+              class="
+                text-lg
+                bg-black
+                rounded-full
+                shadow
+                py-2
+                px-5
+                text-white
+                mt-6
+                transform
+                hover:scale-110
+                hover:bg-blue-600
+                transition
+              "
+            >
+              Shop Now
+            </button>
+          </div>
+        </div>
+        <div class="w-2/5">
+          <img
+            src="https://www.apple.com.cn/v/iphone-12/key-features/c/images/overview/compare/compare_iphone_12_pro__wmdc5a1hns2i_large.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+    <!-- 盒子圆角加阴影 -->
+    <div class="p-8 rounded-xl shadow-md">
+      <div class="flex">
+        <div class="w-3/5 flex flex-col justify-center">
+          <div>
+            <h2 class="text-2xl font-bold">iphone 12 pro</h2>
+            <p class="mt-3">
+              我们的硬件和软件配合默契，协作无间。想将新 AirPods 与 iPhone
+              配对？轻轻一点就设置完毕。想和附近的朋友共享照片、视频或通讯录？隔空投送功能会在屏幕上列出他们的名字，只要动动手指选中就能搞定。
+            </p>
+            <button
+              class="
+                text-lg
+                bg-black
+                rounded-full
+                shadow
+                py-2
+                px-5
+                text-white
+                mt-6
+                transform
+                hover:scale-110
+                hover:bg-blue-600
+                transition
+              "
+            >
+              Shop Now
+            </button>
+          </div>
+        </div>
+        <div class="w-2/5">
+          <img
+            src="https://www.apple.com.cn/v/iphone-12/key-features/c/images/overview/compare/compare_iphone_12_pro__wmdc5a1hns2i_large.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+    <!-- 盒子圆角加阴影 -->
+    <div class="p-8 rounded-xl shadow-md">
+      <div class="flex">
+        <div class="w-3/5 flex flex-col justify-center">
+          <div>
+            <h2 class="text-2xl font-bold">iphone 12 pro</h2>
+            <p class="mt-3">
+              我们的硬件和软件配合默契，协作无间。想将新 AirPods 与 iPhone
+              配对？轻轻一点就设置完毕。想和附近的朋友共享照片、视频或通讯录？隔空投送功能会在屏幕上列出他们的名字，只要动动手指选中就能搞定。
+            </p>
+            <button
+              class="
+                text-lg
+                bg-black
+                rounded-full
+                shadow
+                py-2
+                px-5
+                text-white
+                mt-6
+                transform
+                hover:scale-110
+                hover:bg-blue-600
+                transition
+              "
+            >
+              Shop Now
+            </button>
+          </div>
+        </div>
+        <div class="w-2/5">
+          <img
+            src="https://www.apple.com.cn/v/iphone-12/key-features/c/images/overview/compare/compare_iphone_12_pro__wmdc5a1hns2i_large.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, reactive, toRef, toRefs } from  'vue'
+import { ref, reactive, toRef, toRefs } from 'vue'
 
-  const userF = () => {
-    const state = reactive({
-      foo: 1,
-      bar: 2
-    })
+const userF = () => {
+  const state = reactive({
+    foo: 1,
+    bar: 2
+  })
 
-    return toRefs(state)
-  }
+  return toRefs(state)
+}
 
 export default {
   name: "toRef",
   setup () {
 
-    const { foo, bar} = userF()
+    const { foo, bar } = userF()
     console.log(foo)
     console.log(bar.value)
 
-    const obj  = {
+    const obj = {
       name: "mr.lee",
       age: 100
     }
@@ -29,18 +149,18 @@ export default {
     const info = {
       // name: obj.name,
       // name: ref(obj.name)
-      name: toRef(obj,'name')
+      name: toRef(obj, 'name')
     }
 
-    setTimeout(()=> {
+    setTimeout(() => {
       console.log(info.name.value)
-    },1000)
+    }, 1000)
 
     obj.name = "mr.wang"
 
     console.log(obj.name)
-    const nameRef =  ref(obj.name)
-    const nameToRef = toRef( obj ,'name')
+    const nameRef = ref(obj.name)
+    const nameToRef = toRef(obj, 'name')
     console.log(nameRef.value)
     console.log(nameToRef.value)
     // ref和toRef都是给一个静态数据实行响应式,转换成ref对象
@@ -48,7 +168,7 @@ export default {
     // toRef是引用操作，当数据修改时，原数据也会被修改
 
     // nameRef.value = "mr.wang"
-    nameToRef.value  = "mrl.wang"
+    nameToRef.value = "mrl.wang"
     console.log(obj.name)
 
   }
@@ -56,5 +176,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
--- vite-01/tailwind.config.js
+++ vite-01/tailwind.config.js
@@ -2,7 +2,29 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], 
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'b-gray': 'red',
+      },
+      width: {
+        '24px': '28px',
+        '750px': '750px',
+        '1024px': '1024px'
+      },
+      height: {
+        '24px': '28px',
+        '697px': '697px'
+      },
+      fontSize: {
+        komabig: ['10rem']
+      },
+      fontFamily: {
+        komafont: ['Zhi Mang Xing']
+      },
+      backgroundSize: {
+       '24': '2rem',
+      }
+    },
   },
   variants: {
     extend: {},
